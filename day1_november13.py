79bb82eae8524f140a7c90cfe0990d79cc0fd01e
project(section1) <>
2021-11-13 23:28:29 +0900

    baekjoon bronze1

--- day1_november13.py
+++ day1_november13.py
@@ -1,1 +1,7 @@
-print('Today is first day')
+num = int(input())
+grades_int = list(map(int, input().split()))
+
+max_grade = max(grades_int)
+new_grade = [i / max_grade * 100 for i in grades_int]
+
+print(sum(new_grade) / num)

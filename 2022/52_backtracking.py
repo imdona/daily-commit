430385cd9bfc3603a463520c7427a72972a76d0c
임민정 <imj1995@icloud.com>
2022-03-24 11:17:41 +0900

    백트래킹2

--- 2022/52_backtracking.py
+++ 2022/52_backtracking.py
@@ -18,3 +18,28 @@ from itertools import permutations
 N, M = map(int, input().split())
 li = map(str, range(1, N+1))
 print('\n'.join(list(map(' '.join, permutations(li, M)))))  # 각 수열을 공백으로 구분하고, 줄바꿈하여 출력
+
+
+# -------------------------------------------------------------
+# 15650 [N과 M(2)] - 백트래킹
+# case 1
+'''
+깊이 우선 탐색(DFS)
+참고 : https://zidarn87.tistory.com/331
+추후 다시 풀이
+'''
+N, M = map(int, input().split())
+permu = []
+
+def dfs(start):
+    # 리스트의 개수가 M개이면 탈출 후 출력
+    if len(permu) == M:
+        print(" ".join(map(str, permu)))
+
+    for i in range(start, N+1):
+        if i not in permu:
+            permu.append(i)
+            dfs(i+1)
+            permu.pop()
+
+dfs(1)
